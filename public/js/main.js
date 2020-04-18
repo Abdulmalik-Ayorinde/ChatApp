9ad7d1695fb0f3bcd88ecfadfc53b974a772d565
Abdulmalik-Ayorinde <ayo.malik4life@gmail.com>
2020-04-18 12:32:09 +0100

    adding current user to DOM

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,7 @@
 const chatForm = document.getElementById('chat-form')
 const chatMessages = document.querySelector('.chat-messages');
+const roomName = document.getElementById('room-name');
+const userList = document.getElementById('users');
 
 const {username, room } = Qs.parse(location.search, {
     ignoreQueryPrefix: true
@@ -10,6 +12,12 @@ const socket = io();
 
 //Join chatroom 
 socket.emit('joinroom', {username, room});
+
+// Get room and Users
+socket.on('roomUsers', ({room, users}) => {
+    outputRoomName(room);
+    outputUsers(users)
+});
 
 //Server Message
 socket.on('message', message => {
@@ -45,3 +53,16 @@ function outputMessage(message){
     </p>`;
     document.querySelector('.chat-messages').appendChild(div)
 }
+
+// Adding room name
+function outputRoomName(room){
+    roomName.innerText = room;
+}
+
+// Add users to DOM
+function outputUsers(users) {
+    userList.innerHTML = `
+      ${users.map(user => `<li>${user.username}</li>`).join('')}
+    `;
+  }
+  
