01bbf4deb891ee3921dba8a9647b25209ec92364
Abdulmalik-Ayorinde <ayo.malik4life@gmail.com>
2020-04-18 12:33:50 +0100

    getting room user users

--- utils/users.js
+++ utils/users.js
@@ -1,7 +1,11 @@
 const users = [];
 
 function userJoin(id, username, room) {
-    const user = {id, username, room};
+    const user = {
+        id,
+        username,
+        room
+    };
     users.push(user);
 
     return user;
@@ -11,9 +15,25 @@ function getCurrentUser(id) {
     return users.find(user => user.id === id);
 }
 
+// User leaves chat
+function userLeave(id) {
+    const index = users.findIndex(user => user.id === id);
 
+    if (index !== -1) {
+        return users.splice(index, 1)[0];
+    }
+}
+
+// Get room Uersadded 
+function getRoomUsers(room) {
+    return users.filter(user => 
+        user.room === room
+    )
+}
 
 module.exports = {
     userJoin,
-    getCurrentUser
+    getCurrentUser,
+    userLeave,
+    getRoomUsers
 }
